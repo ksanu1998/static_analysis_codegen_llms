af1cef48b3d957a0530bca2cbeb33f0ca21f9249
Abhishek Anand <abhi1aa@outlook.com>
2023-11-13 14:32:50 -0800

    structuring

--- source_directory/question-68-pass-0.py
+++
@@ -1,3 +0,0 @@
-# Find the sum of the first N Centered Dodecagonal Number. 
-def Centered_Dodecagonal_num(n): 
-    return (n * (n + 1) * (2 * n + 1)) / 6
--- source_directory/question-71-pass-0.py
+++
@@ -1,9 +0,0 @@
-# Find the sum of the first N Centered Octadecagonal Numbers. 
-def center_octadecagon_num(n): 
-    return sum(center_octadecagon_nums(n)) 
-
-# Driver Code 
-print("Sum of the first 10 Centered Octadecagonal Numbers:", 
-      center_octadecagon_num(10)) 
-
-# This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
